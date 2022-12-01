a50f4c806b8c5ad40efb2b7ccd7327dc3731021e
Mary McPhee <maryrsmcphee@gmail.com>
2022-12-01 14:25:13 -0500

    set-up, import data

---
+++ constraints.m
@@ -0,0 +1,6 @@
+function [c, ceq] = constraints(u)
+[existing_garden_coords, grocery_store_coords, gathering_place_coords, possible_locations_coords] = import_data();
+
+c = []; 
+ceq = [];
+end 
---
+++ minimize.m
@@ -0,0 +1,10 @@
+lb = [80.4037291, 43.4304841]; 
+ub = [80.4844980, 43.4663359]; 
+x_coord_guess = 80.44;
+y_coord_guess = 43.445; 
+u = [x_coord_guess, y_coord_guess];
+
+options = optimoptions('fmincon','Display','iter'); % Display each iteration
+uopt = fmincon(@objective,u,[],[],[],[],lb,ub,@constraint,options); % Call fmincon function
+
+display(uopt);
